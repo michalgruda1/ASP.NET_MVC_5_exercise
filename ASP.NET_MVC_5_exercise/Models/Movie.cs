07027435a1128299ceb9e093a69022ecdac9c333
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-06-28 15:29:28 +0200

    Added movie db context

--- ASP.NET_MVC_5_exercise/Models/Movie.cs
+++ ASP.NET_MVC_5_exercise/Models/Movie.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace ASP.NET_MVC_5_exercise.Models
 {
@@ -12,5 +10,10 @@ namespace ASP.NET_MVC_5_exercise.Models
 		public DateTime ReleaseDate { get; set; }
 		public string Genre { get; set; }
 		public decimal Price { get; set; }
+	}
+
+	public class MovieDBContext : DbContext
+	{
+		public DbSet<Movie> Movies { get; set; }
 	}
 }
